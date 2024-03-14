88d8941ca9c0fb4932b3b8f82da127de2cd44da4
clodi <ciao@clodi.tech>
2024-03-14 15:26:47 +0100

    render data

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,13 @@ import Image from "next/image";
 import { fetch } from "./lib/action";
 
 export default async function Home() {
-  const data = await fetch();
+  const data: any = await fetch();
+
+  // prepare the data
+  const rivals = data.football.rivals.nextGame;
+  const cap = rivals.cap;
+  const formationKnown = String(rivals.formationKnown);
+  const slug = rivals.slug;
 
   return (
     <div className="parent">
@@ -13,7 +19,11 @@ export default async function Home() {
             priority={true}/>
       </header>
       <div className="left-side"></div>
-      <main>Main Content</main>
+      <main>
+        <div>{slug}</div>
+        <div>next game cap {cap}</div>
+        <div>formation known: {formationKnown}</div>
+      </main>
       <div className="right-side section yellow"></div>
       <footer>
         <div>a project by <a href="https://sonolibero.io" target="_blank">libero</a></div>
