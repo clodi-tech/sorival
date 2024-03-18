26c8463d1610bc6ad01464e8fbd0ab090397e5ab
clodi <ciao@clodi.tech>
2024-03-18 11:04:35 +0100

    simplify

--- app/home/main.tsx
+++ app/home/main.tsx
@@ -22,24 +22,26 @@ export default async function Main() {
         }
     });
 
+    const GameLink = ({ game }: { game: any }) => (
+        <Link href={`/${game.slug}/players`} key={game.id} className="flex justify-center items-center border border-gray-600 rounded-2xl p-3 m-1">
+            <div className="flex flex-col items-center">
+                <Image src={game.game.homeTeam.pictureUrl} alt="home logo" width={SIZE} height={SIZE} />
+                <span>{game.game.homeTeam.shortName}</span>
+            </div>
+            <span className="mx-2">VS</span>
+            <div className="flex flex-col items-center">
+                <Image src={game.game.awayTeam.pictureUrl} alt="away logo" width={SIZE} height={SIZE} />
+                <span>{game.game.awayTeam.shortName}</span>
+            </div>
+        </Link>
+    );
+
     return (
         <main>
             {gamesWithFormation.length > 0 ? (
                 <>
                     <div>upcoming games</div>
-                    {gamesWithFormation.map((game: any) => (
-                        <Link href={`/${game.slug}/players`} key={game.id} className="flex justify-center items-center border border-gray-600 rounded-2xl p-3 m-1">
-                            <div className="flex flex-col items-center">
-                                <Image src={game.game.homeTeam.pictureUrl} alt="home logo" width={SIZE} height={SIZE} />
-                                <span>{game.game.homeTeam.shortName}</span>
-                            </div>
-                            <span className="mx-2">VS</span>
-                            <div className="flex flex-col items-center">
-                                <Image src={game.game.awayTeam.pictureUrl} alt="away logo" width={SIZE} height={SIZE} />
-                                <span>{game.game.awayTeam.shortName}</span>
-                            </div>
-                        </Link>
-                    ))}
+                    {gamesWithFormation.map((game: any) => <GameLink game={game} />)}
                 </>
             ) : (
                 <>
