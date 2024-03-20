dd1c745499976e43d1fc84e58f5b1ddadf599dee
clodi <ciao@clodi.tech>
2024-03-20 15:12:58 +0100

    game as component

--- app/[slug]/players/page.tsx
+++ app/[slug]/players/page.tsx
@@ -1,35 +1,13 @@
 import { startingLineup } from '@/app/lib/actions';
+import Game from '@/app/lib/game';
 import Lineups from '@/app/lib/lineups';
-import {Card, CardHeader, CardFooter, Image, Divider} from "@nextui-org/react";
-
-const SIZE = 30;
 
 export default async function Page({ params }: { params: { slug: string } }) {
     const {competition, homeTeam, awayTeam, topLineups } = await startingLineup(params.slug);
 
     return (
         <main>
-            <Card className="p-1 m-2">
-                <CardHeader className="flex gap-3">
-                    <div>{homeTeam.shortName}</div>
-                    <Image alt="home logo"
-                        width={SIZE} height={SIZE}
-                        src={homeTeam.pictureUrl}
-                        radius="sm"
-                    />
-                    <div>VS</div>
-                    <Image alt="away logo"
-                        width={SIZE} height={SIZE}
-                        src={awayTeam.pictureUrl}
-                        radius="sm"
-                    />
-                    <div>{awayTeam.shortName}</div>
-                </CardHeader>
-                <Divider/>
-                <CardFooter className="flex justify-center items-center p-0">
-                    {competition.displayName}
-                </CardFooter>
-            </Card>
+            <Game competition={competition} homeTeam={homeTeam} awayTeam={awayTeam} />
             <Lineups topLineups={topLineups} />
         </main>
     );
---
+++ app/lib/game.tsx
@@ -0,0 +1,29 @@
+import {Card, CardHeader, CardFooter, Image, Divider} from "@nextui-org/react";
+
+const SIZE = 30;
+
+export default function Game({ homeTeam, awayTeam, competition }: any) {
+    return (
+        <Card className="p-1 m-2">
+            <CardHeader className="flex gap-3">
+                <div>{homeTeam.shortName}</div>
+                <Image alt="home logo"
+                    width={SIZE} height={SIZE}
+                    src={homeTeam.pictureUrl}
+                    radius="sm"
+                />
+                <div>VS</div>
+                <Image alt="away logo"
+                    width={SIZE} height={SIZE}
+                    src={awayTeam.pictureUrl}
+                    radius="sm"
+                />
+                <div>{awayTeam.shortName}</div>
+            </CardHeader>
+            <Divider/>
+            <CardFooter className="flex justify-center items-center p-0">
+                {competition.displayName}
+            </CardFooter>
+        </Card>
+    );
+}
