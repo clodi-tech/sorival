1aac7aa97c77346fa4ccbd5ce25a14c96b7b45a1
clodi <ciao@clodi.tech>
2024-03-20 18:11:29 +0100

    center link

--- app/home/header.tsx
+++ app/home/header.tsx
@@ -10,20 +10,22 @@ export default function Header() {
                     width={50} height={50}
                     priority={true}/>
             </Link>
-            <Popover placement="bottom" offset={10} backdrop='blur'>
-                <PopoverTrigger>
-                    <Button color="primary" variant='faded' size='sm'>feedback</Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-[240px] gap-3 p-3 border border-gray-700">
-                    <Textarea label="thank you for your valuable feedback!" labelPlacement='outside'
-                        variant="bordered" placeholder="tell me what you like or don't like about the app. I will make it better. thank you."
-                        disableAnimation autoFocus disableAutosize radius='sm'
-                        classNames={{
-                        base: "max-w-xs",
-                        input: "resize-y min-h-[80px]",}}/>
-                    <Button color="primary" size='sm'>SEND</Button>
-                </PopoverContent>
-            </Popover>
+            <div style={{ position: 'absolute', left: '50%', transform: 'translateX(30px)' }}>
+                <Popover placement="bottom" offset={10} backdrop='blur'>
+                    <PopoverTrigger>
+                        <Button color="primary" variant='faded' size='sm'>feedback</Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-[240px] gap-3 p-3 border border-gray-700">
+                        <Textarea label="thank you for your valuable feedback!" labelPlacement='outside'
+                            variant="bordered" placeholder="tell me what you like or don't like about the app. I will make it better. thank you."
+                            disableAnimation autoFocus disableAutosize radius='sm'
+                            classNames={{
+                            base: "max-w-xs",
+                            input: "resize-y min-h-[80px]",}}/>
+                        <Button color="primary" size='sm'>SEND</Button>
+                    </PopoverContent>
+                </Popover>
+            </div>
         </header>
     );
 }
