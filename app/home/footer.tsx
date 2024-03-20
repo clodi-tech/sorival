8fb082421b6b5e3f9b72dc59134932062e05ad05
clodi <ciao@clodi.tech>
2024-03-20 18:18:41 +0100

    footer

--- app/home/footer.tsx
+++ app/home/footer.tsx
@@ -1,7 +1,13 @@
+import { Link } from "@nextui-org/react";
+
 export default function Footer() {
     return (
         <footer>
-            <div>a project by <a href='https://sonolibero.io' target='_blank'>libero</a></div>
+            <div>a project by
+                <Link isBlock isExternal showAnchorIcon href="https://sonolibero.io" color="primary">
+                    libero
+                </Link>
+            </div>
             <div>© 2092</div>
         </footer>
     );
