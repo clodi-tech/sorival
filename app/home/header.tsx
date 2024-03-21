0bc6c0a203d49d3449b5eecfe7b33a8658bac978
clodi <ciao@clodi.tech>
2024-03-21 11:54:28 +0100

    hide feedback until fix

--- app/home/header.tsx
+++ app/home/header.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link';
 import Feedback from '../lib/feedback';
-import { Popover, PopoverTrigger, PopoverContent, Button } from '@nextui-org/react';
 
 export default function Header() {
     return (
@@ -11,16 +10,6 @@ export default function Header() {
                     width={50} height={50}
                     priority={true}/>
             </Link>
-            <div style={{ position: 'absolute', left: '50%', transform: 'translateX(30px)' }}>
-                <Popover placement="bottom" offset={10} backdrop='blur'>
-                    <PopoverTrigger>
-                        <Button color="primary" variant='faded' size='sm'>feedback</Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-[300px] gap-3 p-3 border border-gray-700">
-                        <Feedback />
-                    </PopoverContent>
-                </Popover>
-            </div>
         </header>
     );
 }
